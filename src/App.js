a79e52b98f08625d9e27e3f5240fb95603a51e58
Bgstatic <bilgehangecici@gmail.com>
2020-10-23 15:39:26 +0300

    Updated App.js.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
-import "./App.css";
+import "./styles/App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>All right is reserved</footer>
+    </div>
+  );
 }
 
 export default App;
