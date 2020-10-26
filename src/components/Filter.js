960bd27520d3a33c9e0f5ed51f64127d3a3766ff
Bgstatic <bilgehangecici@gmail.com>
2020-10-26 15:11:25 +0300

    Filter.js added.

---
+++ src/components/Filter.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "../styles/Filter.css";
+
+export default class Filter extends Component {
+  render() {
+    return (
+      <div className="filter">
+        <div className="filter-result">{this.props.count} Products</div>
+        <div className="filter-sort">
+          Order{" "}
+          <select value={this.props.size} onChange={this.props.sortProducts}>
+            <option value="">Latest</option>
+            <option value="lowest">Lowest</option>
+            <option value="highest">Highest</option>
+          </select>
+        </div>
+        <div className="filter-size">
+          Filter{" "}
+          <select value={this.props.size} onChange={this.props.filterProducts}>
+            <option value="">ALL</option>
+            <option value="40">40</option>
+            <option value="41">41</option>
+            <option value="42">42</option>
+            <option value="43">43</option>
+            <option value="44">44</option>
+            <option value="45">45</option>
+            <option value="46">46</option>
+            <option value="47">47</option>
+          </select>
+        </div>
+      </div>
+    );
+  }
+}
